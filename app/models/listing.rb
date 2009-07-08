d6a36a2f462efd105bf719c7cd116fb87a2b0754
Punit Rathore <punit@cerebral.(none)>
2009-07-09 00:37:24 +0530

    Testing Listing with Rspec

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,12 +3,11 @@ class Listing < ActiveRecord::Base
   has_many :applied_listings
   validates_presence_of :title, :body
   
-  def self.listings_of_all_users_except (id)
-    listings =  self.find(:all, :conditions => [:user_id != id ])
-  end
+  #def self.listings_of_all_users_except (id)
+  #  listings =  self.find(:all, :conditions => [:user_id != id ])
+  #end
   
-  def self.display
-    listings = find(:all)
-    
-  end
+  #def self.display
+  #  listings = find(:all)
+  #end
 end
