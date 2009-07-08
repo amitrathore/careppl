8b2d4e0bf8f1265a81d516f2c3ea8689d827e956
Punit Rathore <punit@cerebral.(none)>
2009-07-09 00:23:35 +0530

    Testing Listing using Rspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  include AuthenticatedSystem
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
